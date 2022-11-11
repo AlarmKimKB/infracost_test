adf691d61fd77a03a0b6989861a68c0fea32b147
Scott Kim <vnfmssharin@naver.com>
2022-11-11 17:48:52 +0900

    ec2 create

---
+++ test/main.tf
@@ -0,0 +1,22 @@
+provider "aws" {
+  region  = "ap-northeast-2"
+}
+
+terraform {
+  backend "s3" {
+    bucket = "scott-terraform-study-tfstate"
+    key    = "stg/terraform.tfstate"
+    region = "ap-northeast-2"
+    dynamodb_table = "terraform-locks"
+    # encrypt        = true
+  }
+}
+
+resource "aws_instance" "ubuntu" {
+  ami                    = "ami-0e9bfdb247cc8de84"
+  instance_type          = "t2.micro"
+
+  tags = {
+    name = "atlantis-test-instance"
+  }
+}
